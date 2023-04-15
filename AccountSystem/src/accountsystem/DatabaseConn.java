3fdcc04dee2b55acb53efc98221b4b8a9843b47e
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2023-04-15 16:00:27 +0530

    Add files via upload

---
+++ AccountSystem/src/accountsystem/DatabaseConn.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accountsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+ 
+public class DatabaseConn {
+   static Connection con;
+    public static Connection getConn(){
+       try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/accountmanagement","root","");
+        }catch(Exception ee){
+            System.out.println(ee);
+        }
+       return con;
+   }
+}
+
